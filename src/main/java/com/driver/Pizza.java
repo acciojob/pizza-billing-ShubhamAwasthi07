9781fb4a4a804f3ec4c3b14ed24d42b431444e27
Shubham Awasthi_07 <lkoshubham98@gmail.com>
2022-11-22 20:32:24 +0530

    My pizza Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,14 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg) {
+            System.out.println("Base Price Of The Pizza:" + 300 + "\n");
+            this.price = price + 300;
+        }
+        else {
+            System.out.println("Base Price Of The Pizza:" + 400 + "\n");
+            this.price = price + 400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +25,26 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.price = 80 + price;
+        System.out.println("Extra Cheese Added: " + 80 + "\n");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        this.price =  70 + price;
+        System.out.println("Extra Toppings Added: " + 70 + "\n");
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price = 20 + price;
+        System.out.println("Paperbag Added: " + 20 + "\n");
+
     }
 
     public String getBill(){
         // your code goes here
+        bill = String.valueOf(price);
         return this.bill;
     }
 }
